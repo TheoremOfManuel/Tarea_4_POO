457aaf07e001076555592e9839b991439a58592c
- Manuel Gutierrez <mgutierrezt98@gmail.com>
2025-01-26 15:40:46 -0500

    Segundo commit ejercicio 4.7

---
+++ Ejercicio_4.7/src/Animales/Leon.java
@@ -0,0 +1,23 @@
+
+package Animales;
+public class Leon extends Felino {
+    
+    public String getSonido() {
+    return "Rugido";
+    }
+/**
+* Método que devuelve un String con los alimentos de un león
+* @return Un valor String con la alimentación de un león: “Carnívoro”
+*/
+    public String getAlimentos() {
+    return "Carnívoro";
+    }
+    
+    public String getHabitat() {
+    return "Praderas";
+    }
+    
+    public String getNombreCientifico() {
+    return "Panthera leo";
+    }
+}
---
+++ Ejercicio_4.7/src/Animales/Perro.java
@@ -0,0 +1,17 @@
+
+package Animales;
+public class Perro extends Canido {
+    public String getSonido() {
+    return "Ladrido";
+}
+    public String getAlimentos() {
+        return "Carnívoro";
+}
+    public String getHabitat() {
+        return "Domestico";
+}
+    public String getNombreCientifico() {
+        return "Canis lupus familiaris";
+}
+    
+}
