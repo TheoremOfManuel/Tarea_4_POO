2a92793c33610d1ac79792bc51ae8b68cb55694d
- Manuel Gutierrez <mgutierrezt98@gmail.com>
2025-01-26 22:16:26 -0500

    cuarto commit ejercicio 8.1

---
+++ Ejercicio_8.1/src/Personas/Persona.java
@@ -0,0 +1,16 @@
+
+package Personas;
+public class Persona {
+    String nombre; 
+    String apellidos; 
+    String telefono; 
+    String direccion; 
+    public Persona(String nombre, String apellidos, String teléfono,
+    String dirección) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.telefono = teléfono;
+    this.direccion = dirección;
+}
+    
+}
---
+++ Ejercicio_8.1/src/Personas/Principal.java
@@ -0,0 +1,13 @@
+
+package Personas;
+
+public class Principal {
+
+    public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+    miVentanaPrincipal.setVisible(true); 
+    
+}
+    }
+    
+
